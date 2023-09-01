bb174ec137c2123c715b4a6f2186532dee988077
suchi3656@gmail.com <suchi3656@gmail.com>
2023-09-01 03:57:32 +0000

    added delivery team seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -13,12 +13,16 @@ c.customername,
 c.segment,
 c.country,
 --from raw product
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid','p.productid']) }} as sk_orders,
 p.productid,
 p.category,
 p.productname,
-p.subcategory
+p.subcategory,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d
+on o.shipmode = d.shipmode
